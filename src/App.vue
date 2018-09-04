55cfd11da24685bd998d554c2a3c9fd1994fe19f
ddrohan <ddrohan@wit.ie>
2018-09-04 13:31:30 +0100

    MB - Minor updates for deployment

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,13 @@
           <b-nav-item to="/#"><i class="fa fa-home" style="padding: 5px"> Home</i></b-nav-item>
           <b-nav-item to="/donations"><i class="fa fa-list" style="padding: 5px"> Manage Donations</i></b-nav-item>
           <b-nav-item to="/donate"><i class="fa fa-money" style="padding: 5px"> Donate</i></b-nav-item>
-          <b-nav-item to="/map"><i class="fa fa-globe" style="padding: 5px"> Map</i></b-nav-item>
+          <b-nav-item><i class="fa fa-globe" style="padding: 5px"> Map</i></b-nav-item>
         </b-navbar-nav>
         <b-navbar-nav class="ml-auto">
           <b-nav-item to="/about"><i class="fa fa-info" style="padding: 5px"> About Us</i></b-nav-item>
           <b-nav-item to="/contact"><i class="fa fa-comment" style="padding: 5px"> Contact Us</i></b-nav-item>
-          <b-nav-item to="/login"><i class="fa fa-sign-in" style="padding: 5px"> Login </i></b-nav-item>
-          <b-nav-item to="/logout"><i class="fa fa-sign-out" style="padding: 5px"> Logout </i></b-nav-item>
+          <b-nav-item><i class="fa fa-sign-in" style="padding: 5px"> Login </i></b-nav-item>
+          <b-nav-item><i class="fa fa-sign-out" style="padding: 5px"> Logout </i></b-nav-item>
           <i class="fa fa-pied-piper-alt fa-1x" style="padding: 5px; color: white;"></i>
         </b-navbar-nav>
       </b-collapse>
@@ -38,7 +38,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   padding-top: 20px;
-  background: url("./assets/homerbgyellow.png") no-repeat center top;
+  background: url("./assets/homerbgyellow.png") repeat center top;
 }
 .VueTables__sortable {
   cursor: pointer;
--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default() => {
   return axios.create({
     // baseURL: 'http://localhost:3000/'
-    baseURL: 'https://donationweb-server.herokuapp.com/'
+    baseURL: 'https://donationweb-ssd-nodeserver.herokuapp.com/'
   })
 }
